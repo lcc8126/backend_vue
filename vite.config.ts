07aac7600eec49102442a217e2fcbf9ebb3908d2
lcc8126 <1247564629@qq.com>
2022-11-15 10:36:27 +0800

    初始化

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig, loadEnv, type ConfigEnv } from "vite";
+import {resolve} from 'path'
+
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+// https://vitejs.dev/config/
+export default defineConfig((mode: ConfigEnv) => {
+  const env = loadEnv(mode.mode, process.cwd());
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      // 配置路径别名
+      alias: {
+        '@': resolve(__dirname, "src"),
+      },
+      // 配置文件扩展名
+      extensions: [".ts", ".vue", ".js", ".jsx", ".tsx"], // 导入时想要省略的扩展名列表。
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // 引入 var.scss 这样就可以在全局中使用 var.scss中预定义的变量了
+          // 给导入的路径最后加上 ;
+          additionalData: '@import "./src/styles/variables.scss";',
+        },
+      },
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: "internal:charset-removal",
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === "charset") {
+                  atRule.remove();
+                }
+              },
+            },
+          },
+        ],
+      },
+    },
+  };
+});
